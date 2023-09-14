673df78795e9572bb036cb2945301b4f16cb94e5
ZakiyyullohDev <141619922+ZakiyyullohDev@users.noreply.github.com>
2023-09-14 18:31:18 +0500

    int commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+function updateWatch() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    document.getElementById('watch').textContent = timeString;
+}
+
+setInterval(updateWatch, 1000); // Update every second
+updateWatch(); // Initial call to display the time immediately
